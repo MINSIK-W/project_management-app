a63952418ef5ebc4784183134fb87428ead3f526
minsik90 <minsik90@icloud.com>
2024-08-01 08:47:31 +0900

    "Separating components for JSX and Tailwind styles (enhancing reusability)"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Sidebar from './components/Sidebar.jsx';
 import NewProject from './components/NewProject.jsx';
+import NoProjectSelected from './components/NoProjectSelected.jsx';
 
 function App() {
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
         <Sidebar></Sidebar>
-        <NewProject></NewProject>
+        {/*<NewProject></NewProject>*/}
+        <NoProjectSelected></NoProjectSelected>
       </main>
     </>
   );
