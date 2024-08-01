b555d9552cf557d059e37ae2e24636682980864d
minsik90 <minsik90@icloud.com>
2024-08-01 13:11:06 +0900

    Handling project creation & UI updates

--- src/App.jsx
+++ src/App.jsx
@@ -21,18 +21,18 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
-
-  console.log(projectsState);
 
   let content;
 
@@ -45,7 +45,10 @@ function App() {
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <Sidebar onStartAddProject={handleStartAddProject}></Sidebar>
+        <Sidebar
+          onStartAddProject={handleStartAddProject}
+          projects={projectsState.projects}
+        ></Sidebar>
         {content}
       </main>
     </>
