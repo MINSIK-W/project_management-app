dbbd408bfbe9cab7aeabc5550be52da1d0b982d9
minsik90 <minsik90@icloud.com>
2024-08-01 09:47:22 +0900

    State management for component interchange

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
+import { useState } from 'react';
 import Sidebar from './components/Sidebar.jsx';
-import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
+import NewProject from './components/NewProject.jsx';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((pState) => {
+      return {
+        ...pState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <Sidebar></Sidebar>
-        {/*<NewProject></NewProject>*/}
-        <NoProjectSelected></NoProjectSelected>
+        <Sidebar onStartAddProject={handleStartAddProject}></Sidebar>
+        {content}
       </main>
     </>
   );
